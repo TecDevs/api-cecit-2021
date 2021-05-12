2e1080b5f7a94952243e2b689669e67841c9e1b1
ktdral14 <angel.catedral.1410@gmail.com>
2021-05-12 13:59:16 -0500

    ACM environment path

--- src/App/Config.php
+++ src/App/Config.php
@@ -1,11 +1,15 @@
 <?php
 
-$environmentPath = '/APIS-CORAZAI-2.0';
+// Santi
+// $environmentPath = '/APIS-CORAZAI-2.0';
+
 // Kt
 // $environmentPath = '/applications/api-cotacyt-2021';
 
 // cotacyt
 // $environmentPath = '/api-cecit-2021';
 
+// acm
+$environmentPath = '/api-cecit-2021';
 
 $app->setBasePath($environmentPath);
